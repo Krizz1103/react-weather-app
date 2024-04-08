def98ee1918e5a0e847810bcf18c7a238e4f1c29
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-04-08 20:10:33 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This Website was coded by Kristin Kessler and is{" "}
-        <a
-          href="https://github.com/Krizz1103/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-source on Github
-        </a>{" "}
-        and hosted on Netlify.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This Website was coded by Kristin Kessler and is{" "}
+          <a
+            href="https://github.com/Krizz1103/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source on Github
+          </a>{" "}
+          and hosted on Netlify.
+        </footer>
+      </div>
     </div>
   );
 }
