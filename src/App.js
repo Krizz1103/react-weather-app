c9820aa16f3fca6caa0ba6bfa7bbda1fd8354b50
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-04-08 18:20:58 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>Weather App</h1>
+      <footer>
+        This Website was coded by Kristin Kessler and is{" "}
+        <a
+          href="https://github.com/Krizz1103/react-weather-app"
+          target="_blank"
+        >
+          open-source on Github
+        </a>{" "}
+        and hosted on Netlify.
+      </footer>
     </div>
   );
 }
