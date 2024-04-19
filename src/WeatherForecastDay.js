2651ca47b16ccdb7928b15e90f23935180cf8f01
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-04-19 16:07:29 +0800

    Added responsive css

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecastDay.css";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
@@ -24,7 +25,9 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="Forecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="Weather-icon">
+        <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      </div>
       <div className="Forecast-temperature">
         <span className="Forecast-temperature-max">{maxTemperature()}</span>
         <span className="Forecast-temperature-min">{minTemperature()}</span>
